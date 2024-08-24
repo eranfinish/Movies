5ae980037284eab79fc43eb7383f87048d436bf0
Eran Finish <finishe@gmail.com>
2024-08-25 02:29:28 +0300

    commit 1

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ApiService } from 'src/services/api-service';
+import { SearchComponent } from 'src/components/search/search.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
+
   ],
   imports: [
     BrowserModule,
